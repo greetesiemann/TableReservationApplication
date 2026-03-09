31bfa7374c957c7f9a8cf851394fbe1ce9448e5e
greetesiemann <greete.siemann@gmail.com>
2026-03-09 20:35:13 +0200

    Initial commit

---
+++ src/main/java/ee/siemann/laudadebroneerimine/LaudadeBroneerimineApplication.java
@@ -0,0 +1,13 @@
+package ee.siemann.laudadebroneerimine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaudadeBroneerimineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaudadeBroneerimineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ee/siemann/laudadebroneerimine/LaudadeBroneerimineApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.siemann.laudadebroneerimine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaudadeBroneerimineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
